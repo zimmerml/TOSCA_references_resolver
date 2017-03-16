3a8429a277e9a9af608b2d1c39fc7d126292f78c
Yaroslav Nalivayko <yaroslav.nalivayko@gmail.com>
2017-03-16 14:55:15 +0100

    Maven license headers

---
+++ src/tosca/xml_definitions/RR_TypeImplementation.java
@@ -0,0 +1,155 @@
+package tosca.xml_definitions;
+
+/*-
+ * #%L
+ * TOSCA_RR
+ * %%
+ * Copyright (C) 2017 Stuttgart Uni, IAAS
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import tosca.Control_references;
+
+/**
+ * @author Yaroslav Template Implementation for packages
+ */
+public class RR_TypeImplementation {
+	public static final String extension = "_Impl.tosca";
+
+	/**
+	 * @author Yaroslav  Template Implementation description
+	 */
+	@XmlRootElement(name = "tosca:Definitions")
+	@XmlAccessorType(XmlAccessType.PUBLIC_MEMBER)
+	public static class Definitions {
+
+		@XmlElement(name = "tosca:Import", required = true)
+		public Import import_script;
+		@XmlElement(name = "tosca:Import", required = true)
+		public Import import_IA;
+		@XmlElement(name = "NodeTypeImplementation", required = true)
+		public NodeTypeImplementation nodeTypeImplementation;
+
+		@XmlAttribute(name = "xmlns:tosca", required = true)
+		public static final String tosca="http://docs.oasis-open.org/tosca/ns/2011/12";
+		@XmlAttribute(name = "xmlns:winery", required = true)
+		public static final String winery="http://www.opentosca.org/winery/extensions/tosca/2013/02/12";
+		@XmlAttribute(name = "xmlns:ns1", required = true)
+		public static final String ns1="http://www.eclipse.org/winery/model/selfservice";
+		@XmlAttribute(name = "id", required = true)
+		public static final String id = "winery_RR_NodeType_Impl";
+		@XmlAttribute(name = "targetNamespace", required = true)
+		public static final String targetNamespace="http://opentosca.org/nodetypeimplementations"; //TODO
+		
+		public Definitions() {
+			nodeTypeImplementation = new NodeTypeImplementation();
+			import_script = new Import(RR_ScriptArtifactType.Definitions.ArtifactType.targetNamespace,
+					RR_ScriptArtifactType.filename,"http://docs.oasis-open.org/tosca/ns/2011/12" );
+			import_IA = new Import(RR_ScriptArtifactTemplate.Definitions.targetNamespace,
+					RR_ScriptArtifactTemplate.filename,"http://docs.oasis-open.org/tosca/ns/2011/12" );
+		}
+
+		
+		public static class NodeTypeImplementation {
+
+			@XmlElement(name = "tosca:ImplementationArtifacts", required = true)
+			public ImplementationArtifacts implementationArtifacts;
+			
+			@XmlAttribute(name = "xmlns:ns0", required = true)
+			public static final String ns0 = RR_NodeType.Definitions.targetNamespace;
+			@XmlAttribute(name = "name", required = true)
+			public static final String name = "RR_NodeType_Impl";
+			@XmlAttribute(name = "targetNamespace", required = true)
+			public static final String targetNamespace = "http://opentosca.org/nodetypeimplementations";
+			@XmlAttribute(name = "nodeType", required = true)
+			public static final String nodeType = "ns0:" + RR_NodeType.Definitions.NodeType.name;
+
+			NodeTypeImplementation() {
+				implementationArtifacts = new ImplementationArtifacts();
+			}
+
+			public static class ImplementationArtifacts {
+
+				@XmlElement(name = "tosca:ImplementationArtifact", required = true)
+				public ImplementationArtifact implementationArtifact;
+
+
+				ImplementationArtifacts() {
+					implementationArtifact = new ImplementationArtifact();
+				}
+
+				public static class ImplementationArtifact{
+					@XmlAttribute(name = "xmlns:tbt", required = true)
+					public static final String tbt = RR_ScriptArtifactTemplate.Definitions.ArtifactTemplate.tbt;
+					@XmlAttribute(name = "xmlns:ns6", required = true)
+					public static final String ns6 = RR_ScriptArtifactType.Definitions.targetNamespace;
+					@XmlAttribute(name = "name", required = true)
+					public static final String name = RR_ScriptArtifactTemplate.Definitions.ArtifactTemplate.id;
+					@XmlAttribute(name = "interfaceName", required = true)
+					public static final String interfaceName = RR_NodeType.Definitions.NodeType.Interfaces.Interface.name;
+					@XmlAttribute(name = "operationName", required = true)
+					public static final String operationName = RR_NodeType.Definitions.NodeType.Interfaces.Interface.Operation.name;
+					@XmlAttribute(name = "artifactType", required = true)
+					public static final String artifactType = "tbt:" + RR_ScriptArtifactTemplate.Definitions.ArtifactTemplate.type;
+					@XmlAttribute(name = "artifactRef", required = true)
+					public static final String artifactRef = "ns6:" + RR_ScriptArtifactTemplate.Definitions.ArtifactTemplate.id;
+					ImplementationArtifact() {
+					}
+				}
+			}
+		}
+	}
+	// output filename
+	public static final String filename = "RR_NodeType_Impl.tosca";
+
+	/** Create Type Implementation for my Node Type
+	 * @param cr 
+	 * @throws IOException
+	 * @throws JAXBException
+	 */
+	public static void init(Control_references cr)
+			throws IOException, JAXBException {
+		System.out.println("creating Implementation");
+
+		File temp = new File(cr.getFolder() + Control_references.Definitions + filename);
+		if (temp.exists())
+			temp.delete();
+		temp.createNewFile();
+		OutputStream output = new FileOutputStream(temp);
+
+		JAXBContext jc = JAXBContext.newInstance(Definitions.class);
+
+		Definitions template = new Definitions();
+		
+		Marshaller marshaller = jc.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		marshaller.marshal(template, output);
+		cr.metaFile.addFileToMeta(Control_references.Definitions + filename, "application/vnd.oasis.tosca.definitions");
+	}
+}
